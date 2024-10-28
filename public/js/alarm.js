ddc80adeead2c4973981538607f9c41f2805631f
LordThanos001 <94503577+LordThanos001@users.noreply.github.com>
2024-10-28 14:42:31 +0100

    Update alarm.js

--- public/js/alarm.js
+++ public/js/alarm.js
@@ -41,7 +41,7 @@ const scheduleNotifications = (alarms) => {
 // Function to load alarms from the database and display them in the table
 const loadAlarmsFromDatabase = async () => {
   try {
-    const response = await fetch("http://localhost:4040/api/reminders");
+    const response = await fetch("https://suitable-cody-testalarmapp-77be67d2.koyeb.app/api/reminders");
 
     if (response.ok) {
       const alarms = await response.json();
